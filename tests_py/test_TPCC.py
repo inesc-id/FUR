f3b09439c53cf3485f92eb94b0567e9736ff8ec8
Joao Barreto <joao.barreto@tecnico.pt>
2023-12-22 14:13:26 +0000

    test_TPCC with 3 scenarios from our papers

--- tests_py/test_TPCC.py
+++ tests_py/test_TPCC.py
@@ -9,48 +9,56 @@ if __name__ == "__main__":
   params.set_params("-w", [32]) # nb warehouses
   params.set_params("-m", [32]) # max nb warehouses (put the same as -w)
   params.set_params("-t", [1])
-  #First scenario that we tried:
-  params.set_params("-s", [4], True)
-  params.set_params("-d", [4], True)
-  params.set_params("-o", [4], True)
-  params.set_params("-p", [43], True)
-  params.set_params("-r", [45], True)
+  
+  # From SI-HTM paper: mixed scenario
+  # params.set_params("-s", [4], True)
+  # params.set_params("-d", [4], True)
+  # params.set_params("-o", [4], True)
+  # params.set_params("-p", [43], True)
+  # params.set_params("-r", [45], True)
+
+  # From SI-HTM paper: read-dominated scenario
+  # params.set_params("-s", [4], True)
+  # params.set_params("-d", [4], True)
+  # params.set_params("-o", [80], True)
+  # params.set_params("-p", [4], True)
+  # params.set_params("-r", [8], True)
   
   # From SPHT paper
-  # params.set_params("-s", [0], True)
-  # params.set_params("-o", [0], True)
-  # params.set_params("-p", [95], True)
-  # params.set_params("-r", [2], True)
-  # params.set_params("-d", [3], True)
+  params.set_params("-s", [0], True)
+  params.set_params("-o", [0], True)
+  params.set_params("-p", [95], True)
+  params.set_params("-r", [2], True)
+  params.set_params("-d", [3], True)
 
   # params.set_params("-s", [4, 8], True) # to pass more than 1 combination of values
   # params.set_params("-d", [4, 4], True)
   # params.set_params("-o", [4, 4], True)
   # params.set_params("-p", [43, 39], True)
   # params.set_params("-r", [45, 45], True)
-  params.set_params("-n", [1, 2, 4, 8, 16, 32])
-  # params.set_params("-n", [1, 2, 4, 6, 8, 10, 12, 14, 16, 18, 20, 22, 24, 26, 28, 30, 32])
+  # params.set_params("-n", [1, 2, 4, 8, 16, 32, 64])
+  params.set_params("-n", [1, 2, 4, 6, 8, 10, 12, 14, 16, 18, 20, 22, 24, 26, 28, 30, 32, 34, 36, 38, 40, 42, 44, 46, 48, 50, 52, 54, 56, 58, 60, 62, 64])
   nb_samples = 1
   locations = [
     "../POWER8TM/benchmarks/tpcc",
-    # "../POWER8TM/benchmarks/tpcc",
-    # "../POWER8TM/benchmarks/tpcc",
+    "../POWER8TM/benchmarks/tpcc",
+    "../POWER8TM/benchmarks/tpcc",
     # "../power8tm-pisces/benchmarks/tpcc",
-    # # "../POWER8TM/benchmarks/tpcc",
-    # "../POWER8TM/benchmarks/tpcc",
+    "../POWER8TM/benchmarks/tpcc",
+    "../POWER8TM/benchmarks/tpcc",
     # "../POWER8TM/benchmarks/tpcc",
     # "../POWER8TM/benchmarks/tpcc",
   ]
   # The backend name goes here (don't forget to match the position in the
   # "backends" list with the position in the "locations" list)
   backends = [
-    # "psi",
-    # "psi-strong",
+    "psi",
+    "psi-strong",
     "spht",
     # "pisces",
-    # "htm-sgl",
+    "htm-sgl",
     # "htm-sgl-sr",
-    # "si-htm",
+    "si-htm",
     # "ureads-strong",
     # "ureads-p8tm"
   ]
