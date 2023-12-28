137f1fa1630195caa62e4c5e96886cd655332924
dcastro <daniel.castro@tecnico.ulisboa.pt>
2023-12-28 12:35:27 +0000

    added plot_break_y.py

--- tests_py/plot_break_y.py
+++ tests_py/plot_break_y.py
@@ -2,8 +2,6 @@ import matplotlib.pyplot as plt
 import numpy as np
 import pandas as pd
 
-# TODO: come up with some more automatic way of doing this
-
 # Read data
 files = [
     "dataH1H2_test/pisces-s0.csv",
@@ -28,7 +26,7 @@ Ys = []
 for df in read_all:
     d = df
     d = d[d["-u"] == 50]
-    d = d[d["-i"] == 1000000]
+    d = d[d["-i"] == 200000]
     #breakpoint()
     x = d["-n"]
     y = d["-d"]/d["time"]
@@ -45,10 +43,34 @@ for df in read_all:
 # Create the first subplot
 fig, axs = plt.subplots(nrows=2, ncols=1, sharex=True, gridspec_kw={'height_ratios': [1, 3]})
 axs[0].spines.bottom.set_visible(False)
+axs[0].xaxis.tick_top()
+#axs[0].spines.top.set_visible(False)
 axs[1].spines.top.set_visible(False)
 #axs[0].tick_params(labeltop=False)
 
-# Plot the first segment
+kwargs = dict(marker=[(-1, -0.5), (1, 0.5)], markersize=12,
+              linestyle="none", color='k', mec='k', mew=1, clip_on=False)
+
+# 200000 items
+max_y = 8.0e6
+break_y = 2.0e6
+break_y_offset = 0.1e6
+
+# 50000 items
+#max_y = 2.0e7
+#break_y = 4.5e6
+#break_y_offset = 0.1e6
+
+# 1M items
+#max_y = 5.0e5
+#break_y = 2.0e5
+#break_y_offset = 0.1e5
+
+axs[0].set_ylim([break_y+break_y_offset,max_y]) # top
+axs[1].set_ylim([0,break_y]) # below
+
+axs[0].plot([0,64], [break_y,break_y], **kwargs)
+axs[1].plot([0,64], [break_y,break_y], **kwargs)
 
 for x,y,s in zip(Xs,Ys,solutions):
     axs[1].plot(x, y, label=s)
@@ -57,23 +79,13 @@ axs[1].set_ylabel('Throughput (TXs/s)')
 axs[1].set_xlabel('Threads')
 #axs[1].set_xticklabels([])
 
-# 200000 items
-#axs[0].set_ylim([2.0e6,8.0e6]) # below
-#axs[1].set_ylim([0,2.0e6]) # top
-
-# 50000 items
-#axs[0].set_ylim([4.0e6,2.0e7]) # below
-#axs[1].set_ylim([0,4.0e6]) # top
-
-# 1M items
-axs[0].set_ylim([2.0e5,5.0e5]) # below
-axs[1].set_ylim([0,2.0e5]) # top
-
 # Adjust layout to prevent clipping of the second y-axis label
 plt.tight_layout()
-plt.subplots_adjust(hspace=0, wspace=0.05)
+plt.subplots_adjust(hspace=0.05, wspace=0.05)
+axs[0].margins(x=0)
+axs[1].margins(x=0)
 
 # Show the plot
 axs[1].legend()
-plt.savefig(f"hashmap_50_upts_1M_items.pdf")
+plt.savefig(f"hashmap_50_upts_200k_items.pdf")
 #plt.show()
