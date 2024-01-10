24fc00400af22fa95d084e22f025fe961e6a3c68
Joao Barreto <joao.barreto@tecnico.pt>
2024-01-10 18:28:09 +0000

    hashmap tests also varying buckets

--- tests_py/test_H1_H2.py
+++ tests_py/test_H1_H2.py
@@ -13,14 +13,15 @@ if __name__ == "__main__":
   
   # This class keeps the parameters for the benchmark.
   # Pass a list of arguments that need to be passed to the bechmark during the experiment
-  params = BenchmarkParameters(["-u", "-d", "-i", "-r", "-n"])
+  params = BenchmarkParameters(["-u", "-d", "-i", "-r", "-n", "-b"])
 
   # Here set the possible values for each parameter (pass a list with valid values).
   # Note the experiment will run all possible combinations of arguments.
-  params.set_params("-u", [1, 10, 50, 90])
+  params.set_params("-u", [1, 10])
+  params.set_params("-b", [32, 64, 128])
   # params.set_params("-d", [2000])
-  params.set_params("-d", [600000])
-  params.set_params("-i", [50000, 100000, 200000, 400000, 800000])
+  params.set_params("-d", [6000000])
+  params.set_params("-i", [50000, 200000, 800000])
   # params.set_params("-i", [1000])
   params.set_params("-r", [2000000])
   # params.set_params("-n", [1, 2, 4, 6, 8, 10, 12, 14, 16, 18, 20, 22, 24, 26, 28, 30, 32])
@@ -38,8 +39,8 @@ if __name__ == "__main__":
     "../POWER8TM/benchmarks/datastructures",
     "../POWER8TM/benchmarks/datastructures",
     "../power8tm-pisces/benchmarks/datastructures",
-    # "../POWER8TM/benchmarks/datastructures",
-    # "../POWER8TM/benchmarks/datastructures",
+    "../POWER8TM/benchmarks/datastructures",
+    "../POWER8TM/benchmarks/datastructures",
     # "../POWER8TM/benchmarks/datastructures",
     # "../POWER8TM/benchmarks/datastructures",
   ]
@@ -51,9 +52,9 @@ if __name__ == "__main__":
     "spht",
     "spht-log-linking",
     "pisces",
-    # "htm-sgl",
+    "htm-sgl",
     # "htm-sgl-sr",
-    # "si-htm",
+    "si-htm",
     # "ureads-strong",
     # "ureads-p8tm"
   ]
@@ -94,7 +95,7 @@ if __name__ == "__main__":
         )
       
       # This line starts the benchmark and tests all combinations parameters.
-      #data.run_sample(params) # NOTE: comment if you already have the data and just want to refresh the plots.
+      data.run_sample(params) # NOTE: comment if you already have the data and just want to refresh the plots.
 
       # Parses the stdout into a .csv that can be used for the plots.
       # Check the tests_py/parse_sol.py for the regular expressions that are catched in the stdout.
@@ -149,20 +150,24 @@ if __name__ == "__main__":
             "other": lambda e: e["other-aborts"] + e["rot-other-aborts"]
           })
 
-          # Adds a bar plot for the profile information.
-          # def normalize(e, attr):
-          #   attrs = ["total-wait-time", "total-flush-time", "total-wait2-time", "total-commit-time", "total-abort-time"]
-          #   do_sum = 0.00001
-          #   for s in attrs:
-          #     do_sum += (e[s]/e["-n"]) / (e["total-sum-time"])
-          #   return (e[attr] / e["-n"]) / (e["total-sum-time"]) / do_sum
-          # ds.add_stack("Profile information", "fraction of time", {
-          #   "wait1": lambda e: normalize(e, "total-wait-time"),
-          #   "sus-res": lambda e: normalize(e, "total-flush-time"),
-          #   "wait2": lambda e: normalize(e, "total-wait2-time"),
-          #   "commitTX": lambda e: normalize(e, "total-commit-time"),
-          #   "abortedTX": lambda e: normalize(e, "total-abort-time")
+          # # Adds a bar plot for the profile information.
+          # def divByNumUpdTxs(e, attr):
+          #   return (e[attr] / (e["htm-commits"]+e["rot-commits"]))
+          # ds.add_stack("Latency profile (update txs)", "Time (clock ticks)", {
+          #   "tx proc.": lambda e: divByNumUpdTxs(e, "total-upd-tx-time"),
+          #   "isolation wait": lambda e: divByNumUpdTxs(e, "total-sus-time"),
+          #   "redo log flush": lambda e: divByNumUpdTxs(e, "total-flush-time"),
+          #   "durability wait": lambda e: divByNumUpdTxs(e, "total-dur-commit-time")
           # })
+
+          # # Adds a bar plot for the profile information.
+          # def divByNumROTxs(e, attr):
+          #   return (e[attr] / (e["read-commits"]))
+          # ds.add_stack("Latency profile (read-only txs)", "Time (clock ticks)", {
+          #   "tx proc.": lambda e: divByNumUpdTxs(e, "total-ro-tx-time"),
+          #   "durability wait": lambda e: divByNumUpdTxs(e, "total-ro-dur-wait-time")
+          # })
+
         datasets_thr[u][i] += [ds]
     
   # this for-loop does the actual plotting (in the previous ones we are just
