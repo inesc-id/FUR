44b1bb552fbd0532bbcf545ba5ff6a3048c601f2
Joao Barreto <joao.barreto@tecnico.pt>
2024-01-24 13:03:48 +0000

    renamed some tpcc test scripts

--- tests_py/test-tpcc-orderstatus.py
+++ tests_py/test-tpcc-orderstatus.py
@@ -47,11 +47,11 @@ if __name__ == "__main__":
   #params.set_params("-n", [1, 2, 4, 6, 8, 10, 12, 14, 16, 18, 20, 22, 24, 26, 28, 30, 32, 34, 36, 38, 40, 42, 44, 46, 48, 50, 52, 54, 56, 58, 60, 62, 64])
   nb_samples = 1
   locations = [
+    "../POWER8TM/benchmarks/tpcc",
+    "../POWER8TM/benchmarks/tpcc",
+    "../POWER8TM/benchmarks/tpcc",
+    "../POWER8TM/benchmarks/tpcc",
     "../power8tm-pisces/benchmarks/tpcc",
-    "../POWER8TM/benchmarks/tpcc",
-    "../POWER8TM/benchmarks/tpcc",
-    "../POWER8TM/benchmarks/tpcc",
-    "../POWER8TM/benchmarks/tpcc",
     # "../POWER8TM/benchmarks/tpcc",
     # "../POWER8TM/benchmarks/tpcc",
     # "../POWER8TM/benchmarks/tpcc",
@@ -59,11 +59,11 @@ if __name__ == "__main__":
   # The backend name goes here (don't forget to match the position in the
   # "backends" list with the position in the "locations" list)
   backends = [
-    "pisces",
     "psi",
     "psi-strong",
     "spht",
     "htm-sgl",
+    "pisces",
     # "htm-sgl-sr",
     # "si-htm",
     # "ureads-strong",
